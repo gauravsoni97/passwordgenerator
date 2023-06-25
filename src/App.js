5c86a47d0b23a9f36717a3d1fcca3b2d70e34346
gaurav <gauravsoni8414@gmail.com>
2023-06-25 09:46:06 +0530

    first second commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PasswordGenerator from './Components/PasswordGenerator'
+
+const App = () => {
+  return (
+    <div className='max-w-sm w-sm bg-gray-800 border border-gray-700 rounded-lg p-6'><PasswordGenerator/></div>
+  )
+}
+
+export default App
---
+++ src/Components/PasswordGenerator.jsx
@@ -0,0 +1,87 @@
+import { Slider } from "@mui/material";
+import React, { useState } from "react";
+
+const PasswordGenerator = () => {
+  const [sliderValue, setSliderValue] = useState(17);
+
+  const handleSliderValue = (e) => {
+    setSliderValue(e.target.value);
+  };
+
+  return (
+    <>
+      <form>
+        <h1 className="mb-6 text-xl font-medium">Strong Password Generator</h1>
+        <div class="mb-6 flex items-center">
+          <input
+            type="text"
+            id="text"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="random password "
+            required
+          />
+          <button
+            type="button"
+            class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-2 "
+          >
+            Copy
+          </button>
+        </div>
+
+        <h1 className="mb-2 text-md ">Customize Password</h1>
+        <div className="RangeSlider mb-3 flex items-center bg-gray-800 border border-gray-700 rounded-lg p-3">
+          <div className="RangeCount mr-3">{sliderValue}</div>
+          <Slider
+            value={sliderValue}
+            onChange={handleSliderValue}
+            aria-label="Disabled slider"
+          />
+        </div>
+        <div class="flex items-start mb-6">
+          <label class="relative inline-flex items-center cursor-pointer">
+            <input type="checkbox" value="" class="sr-only peer" />
+            <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+              Toggle me
+            </span>
+          </label>
+        </div>
+        <div class="flex items-start mb-6">
+          <label class="relative inline-flex items-center cursor-pointer">
+            <input type="checkbox" value="" class="sr-only peer" />
+            <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+              Toggle me
+            </span>
+          </label>
+        </div>
+        <div class="flex items-start mb-6">
+          <label class="relative inline-flex items-center cursor-pointer">
+            <input type="checkbox" value="" class="sr-only peer" />
+            <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+              Toggle me
+            </span>
+          </label>
+        </div>
+        <div class="flex items-start mb-6">
+          <label class="relative inline-flex items-center cursor-pointer">
+            <input type="checkbox" value="" class="sr-only peer" />
+            <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+              Toggle me
+            </span>
+          </label>
+        </div>
+        <button
+          type="button"
+          class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 w-full"
+        >
+          Generate Password
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default PasswordGenerator;
